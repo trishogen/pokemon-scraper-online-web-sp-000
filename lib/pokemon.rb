c59ff6f2f00b3d77923a87803bfd37a8d46a9b18
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 18:37:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,9 +4,9 @@ class Pokemon
 
   def initialize(id:, name:, type:, db:)
     @id = id
-    @name = :name
-    @type = :type
-    @db = :db
+    @name = name
+    @type = type
+    @db = db
   end
 
   def self.save(name, type, db)
