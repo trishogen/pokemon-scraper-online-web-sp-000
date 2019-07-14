77213573929dd9009011724e804ed778d59acfa1
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 18:25:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,5 +5,22 @@ class Pokemon
   def initialize(id:, name:, type:, db:)
   end
 
+  def self.save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+      SQL
 
+      DB[:conn].execute(sql, self.name, self.type)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE pokemon SET name = ?, type = ? where id = ?"
+    DB[:conn].execute(sql, self.name, self.type, self.id)
+  end
 end
